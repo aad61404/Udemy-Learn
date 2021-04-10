ff63619c2254fdd975e07e53a19c13da8c3fa0fa
aad61404 <asd61404@gmail.com>
2021-04-10 14:00:36 +0800

    server.js fixed

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const { dirname } = require('path');
 const app = express()
-const publicPath = path.join(_-dirname, '..', 'public');
+const publicPath = path.join(__dirname, '..', 'public');
 
 const port =  process.env.PORT || 3000;
 
